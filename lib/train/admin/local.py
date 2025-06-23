ed11727aeb7f0208d9e46db1ba8e1b0b90c2bb2c
PengchengLia <110672990+PengchengLia@users.noreply.github.com>
2025-06-23 19:33:03 +0800

    Add files via upload

---
+++ lib/train/admin/local.py
@@ -0,0 +1,26 @@
+class EnvironmentSettings:
+    def __init__(self):
+        self.workspace_dir = '/data/wangyuhang/lipengcheng/PECNet'    # Base directory for saving network checkpoints.
+        self.tensorboard_dir = '/data/wangyuhang/lipengcheng/PECNet/tensorboard'    # Directory for tensorboard files.
+        self.pretrained_networks = '/data/wangyuhang/lipengcheng/PECNet/pretrained_networks'
+        self.lasot_dir = '/DATA/lipengcheng/code/PECNet/data/lasot'
+        self.got10k_dir = '/DATA/lipengcheng/code/PECNet/data/got10k/train'
+        self.got10k_val_dir = '/DATA/lipengcheng/code/PECNet/data/got10k/val'
+        self.lasot_lmdb_dir = '/DATA/lipengcheng/code/PECNet/data/lasot_lmdb'
+        self.got10k_lmdb_dir = '/DATA/lipengcheng/code/PECNet/data/got10k_lmdb'
+        self.trackingnet_dir = '/DATA/lipengcheng/code/PECNet/data/trackingnet'
+        self.trackingnet_lmdb_dir = '/DATA/lipengcheng/code/PECNet/data/trackingnet_lmdb'
+        self.coco_dir = '/DATA/lipengcheng/code/PECNet/data/coco'
+        self.coco_lmdb_dir = '/DATA/lipengcheng/code/PECNet/data/coco_lmdb'
+        self.lvis_dir = ''
+        self.sbd_dir = ''
+        self.imagenet_dir = '/DATA/lipengcheng/code/PECNet/data/vid'
+        self.imagenet_lmdb_dir = '/DATA/lipengcheng/code/PECNet/data/vid_lmdb'
+        self.lasher_train_dir = '/data/Datasets/LasHeR/' #'/DATA/lipengcheng/dataset/LasHeR/'
+        self.lasher_test_dir = '/data/Datasets/LasHeR/' #'/DATA/lipengcheng/dataset/LasHeR/'
+        self.imagenetdet_dir = ''
+        self.ecssd_dir = ''
+        self.hkuis_dir = ''
+        self.msra10k_dir = ''
+        self.davis_dir = ''
+        self.youtubevos_dir = ''
